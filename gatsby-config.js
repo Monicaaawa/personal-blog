182deff06c661d83c3e1e7fcea08629e76a1d761
Shiyu Ye <105144733+Monicaaawa@users.noreply.github.com>
2023-03-31 21:41:51 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Story of My Life`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
